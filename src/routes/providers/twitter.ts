10461bfad6664630d916bbd0218a85b6a4c5ab24
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-07-04 16:55:24 -0600

    refactor token

--- src/routes/providers/twitter.ts
+++ src/routes/providers/twitter.ts
@@ -22,7 +22,7 @@ interface RevocationResponse {
 
 declare module 'express-session' {
   export interface SessionData {
-    tokenSet: TokenSet;
+    token_set: TokenSet;
     state: string;
     codeVerifier: string;
     originalUrl: string;
@@ -35,13 +35,18 @@ let confidentialClient;
 let publicClient;
 let client;
 
-const getTwiiterSessionToken = async (tokenSet) => {
+const getTwiiterSessionToken = async (token_set) => {
   const { data } = await axios.get<UsersMeResponse>('https://api.twitter.com/2/users/me', {
     headers: {
-      Authorization: `Bearer ${tokenSet.access_token}`,
+      Authorization: `Bearer ${token_set.access_token}`,
     },
   });
-  const token = getSessionToken({ ...data, tokenSet });
+  const token = getSessionToken({
+    user: {
+      ...data.data,
+    },
+    token_set,
+  });
   return token;
 };
 
@@ -67,8 +72,8 @@ if (config.providers.twitter) {
   router.get('/provider/twitter', (req, res, next) => {
     (async () => {
       const redirect_uri = (req.query.redirect_uri as string) || req.originalUrl;
-      if (req.session.tokenSet) {
-        const token = await getTwiiterSessionToken(req.session.tokenSet);
+      if (req.session.token_set) {
+        const token = await getTwiiterSessionToken(req.session.token_set);
         const client_redirect_uri = `${redirect_uri}?token=${token}&provider=twitter`;
         return res.redirect(client_redirect_uri);
       }
@@ -99,16 +104,16 @@ if (config.providers.twitter) {
       const state = req.session.state;
       const codeVerifier = req.session.codeVerifier;
       const params = client.callbackParams(req);
-      const tokenSet = await client.oauthCallback(
+      const token_set = await client.oauthCallback(
         config.providers.twitter.callback_url,
         params,
         { code_verifier: codeVerifier, state },
         { exchangeBody: { client_id: config.providers.twitter.client_id } }
       );
-      console.log('received and validated tokens %j', tokenSet);
-      req.session.tokenSet = tokenSet;
+      console.log('received and validated tokens %j', token_set);
+      req.session.token_set = token_set;
       if (typeof req.session.originalUrl != 'string') throw new Error('originalUrl must be a string');
-      const token = await getTwiiterSessionToken(req.session.tokenSet);
+      const token = await getTwiiterSessionToken(req.session.token_set);
       const client_redirect_uri = `${req.session.redirect_uri}?token=${token}&provider=twitter`;
       return res.redirect(client_redirect_uri);
     })().catch(next);
@@ -116,13 +121,13 @@ if (config.providers.twitter) {
 
   router.get('/provider/twitter/refresh', (req, res, next) => {
     (async () => {
-      if (!req.session || !req.session.tokenSet || !req.session.tokenSet.refresh_token) {
+      if (!req.session || !req.session.token_set || !req.session.token_set.refresh_token) {
         return res.status(403).send('NG');
       }
       const { data } = await axios.post<TokenSet>(
         'https://api.twitter.com/2/oauth2/token',
         {
-          refresh_token: req.session.tokenSet.refresh_token,
+          refresh_token: req.session.token_set.refresh_token,
           grant_type: 'refresh_token',
           client_id: config.providers.twitter.client_id,
         },
@@ -134,20 +139,20 @@ if (config.providers.twitter) {
         }
       );
       console.log(data);
-      req.session.tokenSet = data;
+      req.session.token_set = data;
       return res.send('OK!');
     })().catch(next);
   });
 
   router.get('/provider/twitter/revoke', (req, res, next) => {
     (async () => {
-      if (!req.session.tokenSet) {
+      if (!req.session.token_set) {
         return res.status(403).send('NG');
       }
       const { data } = await axios.post<RevocationResponse>(
         'https://api.twitter.com/2/oauth2/revoke',
         {
-          token: req.session.tokenSet.access_token,
+          token: req.session.token_set.access_token,
           client_id: config.providers.twitter.client_id,
           token_type_hint: 'access_token',
         },
