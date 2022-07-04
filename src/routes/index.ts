882ecddde11889b13ec635b8bada86a72c201fc6
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-07-04 10:34:55 -0600

    add verify token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import { ReasonPhrases } from 'http-status-codes';
 import twitterRouter from './providers/twitter';
+import tokenRouter from './token';
 
 const router = express.Router();
 
 router.get('/healthz', (req, res) => res.send(ReasonPhrases.OK));
 router.use('/', twitterRouter);
+router.use('/', tokenRouter);
 
 export default router;
