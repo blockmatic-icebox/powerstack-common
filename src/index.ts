f057bc163639c51667d80cdef046f9f4beb2b9ad
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-07-06 16:50:32 -0600

    use cors

--- src/index.ts
+++ src/index.ts
@@ -14,8 +14,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
-
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(
   require('express-session')({
     name: 'powerstack-auth',
