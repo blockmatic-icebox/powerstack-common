d7947d3e69e23f0ace6077cc2b9f21c9539619c3
estebanmontes <58562774+estebanmontes@users.noreply.github.com>
2022-07-26 19:49:01 -0600

    fix: get signatures and pub_key (#1)
    Co-authored-by: Esteban <esteban@Esteban.local>

--- src/routes/providers/anchor.ts
+++ src/routes/providers/anchor.ts
@@ -10,9 +10,13 @@ if (config.providers.anchor) {
   router.post('/provider/anchor', (req, res, next) => {
     (async () => {
       try {
-        const { signature, digest, pub_key } = req.body;
-        const eos_signature = eosio.Signature.from(signature);
-        const eos_pub_key = eosio.PublicKey.from(pub_key);
+        const { identity } = req.body;
+        const pub_key = eosio.PublicKey.from(identity.session.publicKey)
+        const { transaction, signatures } = await identity.session.transact()
+        const digest = transaction.signingDigest(identity.session.chainId).toString()
+        const signature = signatures.map((sign) => sign.toString())[1]
+        const eos_signature = eosio.Signature.from(signature)
+        const eos_pub_key = eosio.PublicKey.from(pub_key)
         const is_valid_signature = eos_signature.verifyDigest(digest, eos_pub_key);
         if (!is_valid_signature)
           return res.send({
