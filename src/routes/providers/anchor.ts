f53d89e08140d648f5334012a77cbe914406145e
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-08-04 18:09:22 +0200

    chore: check login with anchor

--- src/routes/providers/anchor.ts
+++ src/routes/providers/anchor.ts
@@ -1,38 +1,40 @@
-import { config } from '../../config';
-import express from 'express';
-import axios from 'axios';
-import { getSessionToken } from './../../library';
-import * as eosio from '@greymass/eosio';
+import { config } from '../../config'
+import express from 'express'
+import axios from 'axios'
+import { getSessionToken } from './../../library'
+import * as eosio from '@greymass/eosio'
 
-const router = express.Router();
+const router = express.Router()
 
 if (config.providers.anchor) {
   router.post('/provider/anchor', (req, res, next) => {
-    (async () => {
+    ;(async () => {
       try {
-        const { signature, digest, pub_key } = req.body;
-        const eos_signature = eosio.Signature.from(signature);
-        const eos_pub_key = eosio.PublicKey.from(pub_key);
-        const is_valid_signature = eos_signature.verifyDigest(digest, eos_pub_key);
+        const { signature, digest, pub_key } = req.body
+        const eos_signature = eosio.Signature.from(signature)
+        const eos_pub_key = eosio.PublicKey.from(pub_key)
+        const is_valid_signature = eos_signature.verifyDigest(digest, eos_pub_key)
         if (!is_valid_signature)
           return res.send({
             token: null,
-          });
+          })
         const token = await getSessionToken({
           user: {
-            account: pub_key,
+            id: '',
+            name: '',
+            username: pub_key,
           },
-        });
+        })
         return res.send({
           token,
-        });
+        })
       } catch (error) {
         res.send({
           token: null,
-        });
+        })
       }
-    })().catch(next);
-  });
+    })().catch(next)
+  })
 }
 
-export default router;
+export default router
