3802abadfca7145807425cbd38db43af682ecf38
Ruben Abarca <70239531+ruge0326@users.noreply.github.com>
2022-08-15 00:56:12 +0200

    Login with metamask (#5)
    * wip: login metamask
    
    * wip: login with metamask
    
    * wip: login with metamask

--- src/routes/providers/evm.ts
+++ src/routes/providers/evm.ts
@@ -8,8 +8,12 @@ const router = express.Router()
 router.post('/provider/evm', async (req, res, next) => {
   try {
     console.log('/provider/evm')
-    const { address: user_address, signed_message, message } = req.body
-    const address_message = await ethers.utils.verifyMessage(message, signed_message)
+    const { address: user_address, signed_message, message, auth_method } = req.body
+    // TODO: improve validation
+    if (auth_method !== 'web3_metamask' && auth_method !== 'web3_auth')
+      return res.status(401).send({ token: null, error: 'Invalid login method' }) // TODO: fix me normalize error
+
+    const address_message = ethers.utils.verifyMessage(message, signed_message)
 
     if (address_message !== user_address)
       return res.status(401).send({ token: null, error: 'Invalid Signature' }) // TODO: fix me normalize error
@@ -17,14 +21,15 @@ router.post('/provider/evm', async (req, res, next) => {
     const token = await getTokenSession({
       username: 'anon', // TODO: fix me
       address: user_address,
-      auth_method: 'web3_evm',
+      auth_method,
     })
     return res.send({
       token: token,
       error: null,
     })
   } catch (error) {
-    res.send({ token: null, error })
+    console.log({ error })
+    return res.status(401).send({ token: null, error: error.message }) // TODO: fix me normalize error
   }
 })
 
