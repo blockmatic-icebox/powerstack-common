671213c94cb87605fbda07cef807f3a31008e2fb
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-08-15 08:53:54 +0200

    wip: add solana login

--- src/routes/providers/solana.ts
+++ src/routes/providers/solana.ts
@@ -9,21 +9,31 @@ const router = express.Router()
 if (config.providers.anchor) {
   router.post('/provider/phantom', async (req, res, next) => {
     try {
-      const { address, signature, message } = req.body
-      const enc = new TextEncoder().encode
-      const resp = nacl.sign.detached.verify(enc(message), enc(signature), enc(address))
-      console.log('auth response', resp)
-      // throw new AuthorizationError(`Invalid signature`)
-      const is_valid_signature = false
-      if (!is_valid_signature) return res.send({ token: null })
+      // TODO: fix me validate body
+      const { address, signature, message, public_key } = req.body
+      const encoder = new TextEncoder().encode
+      const is_valid_signature = nacl.sign.detached.verify(
+        encoder(message),
+        encoder(signature),
+        encoder(public_key),
+      )
+      console.log('auth response is_signature', is_valid_signature)
+      if (!is_valid_signature)
+        return res.status(401).send({ token: null, error: 'Invalid Signature' }) // TODO: fix me normalize error
+
       const token = await getTokenSession({
         address,
-        username: 'solano',
+        username: 'solana', // TODO fix me
         auth_method: 'web3_solana',
+        public_key: public_key,
       })
-      return res.send({ token })
+      return res.send({
+        token: token,
+        error: null,
+      })
     } catch (error) {
-      res.send({ token: null })
+      console.log({ error })
+      return res.status(401).send({ token: null, error: error.message }) // TODO: fix me normalize error
     }
   })
 }
