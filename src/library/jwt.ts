ce8e1ef2c700a0af8567d3d948acf8e376505630
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-25 14:12:35 -0600

    wip: jwt strategy

--- src/library/jwt.ts
+++ src/library/jwt.ts
@@ -1,12 +1,11 @@
 import { verify, sign } from 'jsonwebtoken'
 import { TokenPayload, AuthInfo } from '../types'
 import { config } from '../config'
-import { prisma } from './prisma'
+import { getOrCreateAccount, prisma } from './prisma'
 import { randomUUID } from 'node:crypto'
 
 export const verifyToken = (token: string) => {
   if (!token) return new Error('Invalid token')
-
   let decoded_token
   try {
     decoded_token = verify(token, config.jwt_secret.key)
@@ -20,7 +19,6 @@ export const verifyToken = (token: string) => {
 
 export const getRefreshToken = (token: string, session) => {
   if (!token) return new Error('Invalid token')
-
   try {
     verify(token, config.jwt_secret.key)
   } catch (error) {
@@ -30,52 +28,28 @@ export const getRefreshToken = (token: string, session) => {
     algorithm: config.jwt_secret.type,
     expiresIn: config.jwt_token_expiration,
   })
-
   return new_token
 }
 
 // https://hasura.io/docs/latest/auth/authentication/jwt/
-export const getTokenPayload = async ({
-  login_address,
-  login_network,
-  login_username,
-  login_method,
-}: AuthInfo): Promise<TokenPayload> => {
-  // TODO: fix twitter login
-  const account = await prisma.accounts.findFirst({
-    where: {
-      addresses: {
-        some: {
-          address: login_address,
-          network: login_network,
-        },
-      },
-    },
-    include: {
-      addresses: true,
-    },
-  })
-
-  // TODO: create new account if no account is found
-
+export const getTokenPayload = async (auth_info: AuthInfo): Promise<TokenPayload> => {
+  const account = await getOrCreateAccount(auth_info)
   const session_id = randomUUID()
-
   const token_payload: TokenPayload = {
     user: {
       account_id: account.account_id,
       session_id,
       username: account.username,
-      auth_method: login_method,
+      auth_method: auth_info.login_method,
     },
     'https://hasura.io/jwt/claims': {
       'x-hasura-allowed-roles': ['user'],
       'x-hasura-default-role': 'user',
       'x-hasura-user-account-id': account.account_id,
       'x-hasura-user-username': account.username,
-      'x-hasura-user-auth-method': login_method,
+      'x-hasura-user-auth-method': auth_info.login_method,
     },
   }
-
   return token_payload
 }
 
--- src/library/prisma.ts
+++ src/library/prisma.ts
@@ -1,3 +1,26 @@
 import { PrismaClient } from '@prisma/client'
+import { AuthInfo } from '~/types'
 
 export const prisma = new PrismaClient()
+
+export const getOrCreateAccount = async ({
+  login_address,
+  login_network,
+  login_username,
+  login_method,
+}: AuthInfo) => {
+  const account = await prisma.accounts.findFirst({
+    where: {
+      addresses: {
+        some: {
+          address: login_address,
+          network: login_network,
+        },
+      },
+    },
+    include: {
+      addresses: true,
+    },
+  })
+  return account
+}
